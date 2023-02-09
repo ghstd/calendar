52597456e395c1c2131dbcfe926a7f58e6a2df0e
AntonRm <tworkn1@gmail.com>
2023-02-09 21:34:36 +0200

    v1

--- server.js
+++ server.js
@@ -1,22 +1,30 @@
 const http = require('http');
 const fs = require('fs');
 
+const allUsersUrl = 'https://api.jsonbin.io/v3/b/63e15af7ebd26539d077b53f';
+
+// Server
 const server = http.createServer(async (request, response) => {
 
-	// OPTIONS
-	if (request.method === 'OPTIONS') {
-		response.setHeader('Access-Control-Allow-Origin', '*')
-		response.setHeader('Access-Control-Allow-Methods', '*')
-		response.setHeader('Access-Control-Allow-Headers', '*')
-		response.setHeader('Access-Control-Max-Age', 2592000)
-		response.end()
-		return
+	function _setHeaders(response) {
+		response.setHeader('Access-Control-Allow-Credentials', 'true')
+		response.setHeader('Access-Control-Allow-Origin', request.headers.origin)
+		response.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+		response.setHeader('Access-Control-Allow-Methods', ['OPTIONS', 'GET', 'POST'])
+		response.setHeader('Access-Control-Max-Age', 600)
 	}
 
-	// GET /importantNotes
-	if (request.url === '/importantNotes') {
+	function _parseCookie() {
+		return request.headers.cookie?.split(';').find(str => /useCalendarKey/.test(str))?.trim().split('=')[1];
+	}
+
+	async function _getUrlFromDBByUserKey() {
+		const userKey = _parseCookie();
+
+		if (!userKey) return 'no userKey'
+
 		async function _getDataFromDB() {
-			const response = await fetch('https://api.jsonbin.io/v3/b/63d81542ebd26539d06f4b54', {
+			const response = await fetch(allUsersUrl, {
 				method: 'GET',
 				headers: {
 					'Content-type': 'application/json',
@@ -27,7 +35,204 @@ const server = http.createServer(async (request, response) => {
 			return data.record
 		}
 
-		const serverData = await _getDataFromDB();
+		const usersInDB = await _getDataFromDB();
+		const name = Object.keys(usersInDB).find(name => usersInDB[name].key === userKey);
+
+		return `https://api.jsonbin.io/v3/b/${usersInDB[name].url}`
+	}
+
+	// OPTIONS
+	if (request.method === 'OPTIONS') {
+		_setHeaders(response)
+		response.end()
+		return
+	}
+
+	// GET /checkLogin
+	if (request.url === '/checkLogin') {
+		const userKey = _parseCookie();
+
+		if (!userKey) {
+			_setHeaders(response)
+			response.setHeader('Content-Type', 'text/plain')
+			response.end('no login')
+			return
+		} else {
+			_setHeaders(response)
+			response.setHeader('Content-Type', 'text/plain')
+			response.end('has login')
+			return
+		}
+	}
+
+	// POST /login
+	if (request.url === '/login' && request.method === 'POST') {
+		const body = [];
+
+		request
+			.on('error', (err) => {
+				console.error(err);
+			})
+			.on('data', (chunk) => {
+				body.push(chunk);
+			})
+			.on('end', async () => {
+				async function _getDataFromDB() {
+					const response = await fetch(allUsersUrl, {
+						method: 'GET',
+						headers: {
+							'Content-type': 'application/json',
+							'X-Master-Key': '$2b$10$xaWeFLridtnNorEUAg.CP.6TLeGmu/X/YlKoIIAUh1adEyot8T6ei'
+						}
+					});
+					const data = await response.json();
+					return data.record
+				}
+
+				const usersInDB = await _getDataFromDB();
+				const inputData = JSON.parse(Buffer.concat(body).toString());
+
+				// signin
+				if (inputData.type === 'signin') {
+					const user = Object.keys(usersInDB).find(userName => userName === inputData.name);
+					if (!user) {
+						_setHeaders(response)
+						response.setHeader('Content-Type', 'text/plain')
+						response.end('no name')
+						return
+					}
+
+					const correctPassword = usersInDB[user].password === inputData.password;
+					if (!correctPassword) {
+						_setHeaders(response)
+						response.setHeader('Content-Type', 'text/plain')
+						response.end('no password')
+						return
+					}
+
+					_setHeaders(response)
+					response.setHeader('Content-Type', 'text/plain')
+					response.setHeader('Set-Cookie', [`useCalendarKey=${usersInDB[user].key};Max-Age=259200;Secure;HttpOnly`])
+					response.end('success login')
+				}
+
+				// signup
+				if (inputData.type === 'signup') {
+					if (!inputData.name) {
+						_setHeaders(response)
+						response.setHeader('Content-Type', 'text/plain')
+						response.end('no name')
+						return
+					}
+
+					if (!inputData.password) {
+						_setHeaders(response)
+						response.setHeader('Content-Type', 'text/plain')
+						response.end('no password')
+						return
+					}
+
+					const name = Object.keys(usersInDB).find(name => name === inputData.name);
+
+					if (name) {
+						_setHeaders(response)
+						response.setHeader('Content-Type', 'text/plain')
+						response.end('no name')
+						return
+					}
+
+					const date = new Date();
+					const calendarBasicData = [
+						{
+							"year": date.getFullYear(),
+							"months": [
+								{
+									"month": date.getMonth(),
+									"days": [
+										{
+											"day": date.getDate(),
+											"notes": []
+										}
+									]
+								}
+							]
+						}
+					];
+
+					async function _createNewUserInDB(value) {
+						const response = await fetch('https://api.jsonbin.io/v3/b', {
+							method: 'POST',
+							headers: {
+								'Content-type': 'application/json',
+								'X-Master-Key': '$2b$10$xaWeFLridtnNorEUAg.CP.6TLeGmu/X/YlKoIIAUh1adEyot8T6ei',
+								'X-Bin-Name': inputData.name
+							},
+							body: JSON.stringify(value)
+						});
+						const data = await response.json();
+						return data.metadata.id
+					}
+					const newUserUrl = await _createNewUserInDB(calendarBasicData);
+
+					function _randomKey() {
+						const str = '1234567890abcdefghjklmnopqrstvwxyz';
+						const arr = [];
+						for (let i = 0; i < 10; i++) {
+							arr.push(str[Math.floor(Math.random() * str.length)])
+						}
+						return arr.join('')
+					}
+					const newUserKey = _randomKey();
+
+					usersInDB[inputData.name] = {
+						password: inputData.password,
+						key: newUserKey,
+						url: newUserUrl
+					}
+
+					function _putDataInDB(value) {
+						fetch(allUsersUrl, {
+							method: 'PUT',
+							headers: {
+								'Content-type': 'application/json',
+								'X-Master-Key': '$2b$10$xaWeFLridtnNorEUAg.CP.6TLeGmu/X/YlKoIIAUh1adEyot8T6ei'
+							},
+							body: JSON.stringify(value)
+						})
+					}
+					_putDataInDB(usersInDB)
+
+					_setHeaders(response)
+					response.setHeader('Content-Type', 'text/plain')
+					response.setHeader('Set-Cookie', [`useCalendarKey=${newUserKey};Max-Age=259200;Secure;HttpOnly`])
+					response.end('success login')
+				}
+			})
+	}
+
+	// GET /importantNotes
+	if (request.url === '/importantNotes') {
+		const url = await _getUrlFromDBByUserKey();
+
+		if (url === 'no userKey') {
+			_setHeaders(response)
+			response.end(JSON.stringify({ status: 'no userKey' }))
+			return
+		}
+
+		async function _getDataFromDB(url) {
+			const response = await fetch(url, {
+				method: 'GET',
+				headers: {
+					'Content-type': 'application/json',
+					'X-Master-Key': '$2b$10$xaWeFLridtnNorEUAg.CP.6TLeGmu/X/YlKoIIAUh1adEyot8T6ei'
+				}
+			});
+			const data = await response.json();
+			return data.record
+		}
+
+		const serverData = await _getDataFromDB(url);
 		const importantNotes = [];
 
 		serverData.forEach(year => {
@@ -48,18 +253,23 @@ const server = http.createServer(async (request, response) => {
 			})
 		})
 
-		response.setHeader('Access-Control-Allow-Origin', '*')
-		response.setHeader('Access-Control-Allow-Methods', '*')
-		response.setHeader('Access-Control-Allow-Headers', '*')
-		response.setHeader('Access-Control-Max-Age', 2592000)
+		_setHeaders(response)
 		response.end(JSON.stringify(importantNotes))
 		return
 	}
 
 	// GET /weekTasks
 	if (request.url === '/weekTasks') {
-		async function _getDataFromDB() {
-			const response = await fetch('https://api.jsonbin.io/v3/b/63d81542ebd26539d06f4b54', {
+		const url = await _getUrlFromDBByUserKey();
+
+		if (url === 'no userKey') {
+			_setHeaders(response)
+			response.end(JSON.stringify({ status: 'no userKey' }))
+			return
+		}
+
+		async function _getDataFromDB(url) {
+			const response = await fetch(url, {
 				method: 'GET',
 				headers: {
 					'Content-type': 'application/json',
@@ -70,7 +280,7 @@ const server = http.createServer(async (request, response) => {
 			return data.record
 		}
 
-		const serverData = await _getDataFromDB();
+		const serverData = await _getDataFromDB(url);
 		const date = new Date();
 		const firstWeekday = date.getDate() - ((date.getDay() === 0 ? 7 : date.getDay()) - 1);
 		date.setDate(firstWeekday)
@@ -91,18 +301,23 @@ const server = http.createServer(async (request, response) => {
 			date.setDate(date.getDate() + 1)
 		}
 
-		response.setHeader('Access-Control-Allow-Origin', '*')
-		response.setHeader('Access-Control-Allow-Methods', '*')
-		response.setHeader('Access-Control-Allow-Headers', '*')
-		response.setHeader('Access-Control-Max-Age', 2592000)
+		_setHeaders(response)
 		response.end(JSON.stringify(weekTasks))
 		return
 	}
 
 	// GET /notes
 	if (/notes/.test(request.url) && request.method === 'GET') {
-		async function _getDataFromDB() {
-			const response = await fetch('https://api.jsonbin.io/v3/b/63d81542ebd26539d06f4b54', {
+		const url = await _getUrlFromDBByUserKey();
+
+		if (url === 'no userKey') {
+			_setHeaders(response)
+			response.end(JSON.stringify({ status: 'no userKey' }))
+			return
+		}
+
+		async function _getDataFromDB(url) {
+			const response = await fetch(url, {
 				method: 'GET',
 				headers: {
 					'Content-type': 'application/json',
@@ -113,7 +328,7 @@ const server = http.createServer(async (request, response) => {
 			return data.record
 		}
 
-		const serverData = await _getDataFromDB();
+		const serverData = await _getDataFromDB(url);
 		const urlParams = new URL(request.url, `http://${request.headers.host}`).searchParams;
 		const inputParams = {};
 		for (const [key, value] of urlParams) {
@@ -172,16 +387,13 @@ const server = http.createServer(async (request, response) => {
 			.days
 			.find(item => item.day === inputParams.day);
 
-		response.setHeader('Access-Control-Allow-Origin', '*')
-		response.setHeader('Access-Control-Allow-Methods', '*')
-		response.setHeader('Access-Control-Allow-Headers', '*')
-		response.setHeader('Access-Control-Max-Age', 2592000)
+		_setHeaders(response)
 		response.end(JSON.stringify(outputData))
 		return
 	}
 
-	// POST
-	if (request.method === 'POST') {
+	// POST /notes
+	if (/notes/.test(request.url) && request.method === 'POST') {
 
 		const body = [];
 
@@ -193,8 +405,16 @@ const server = http.createServer(async (request, response) => {
 				body.push(chunk);
 			})
 			.on('end', async () => {
-				async function _getDataFromDB() {
-					const response = await fetch('https://api.jsonbin.io/v3/b/63d81542ebd26539d06f4b54', {
+				const url = await _getUrlFromDBByUserKey();
+
+				if (url === 'no userKey') {
+					_setHeaders(response)
+					response.end(JSON.stringify({ status: 'no userKey' }))
+					return
+				}
+
+				async function _getDataFromDB(url) {
+					const response = await fetch(url, {
 						method: 'GET',
 						headers: {
 							'Content-type': 'application/json',
@@ -205,8 +425,8 @@ const server = http.createServer(async (request, response) => {
 					return data.record
 				}
 
-				const serverData = await _getDataFromDB();
-				const inputData = JSON.parse(body.toString());
+				const serverData = await _getDataFromDB(url);
+				const inputData = JSON.parse(Buffer.concat(body).toString());
 				const urlParams = new URL(request.url, `http://${request.headers.host}`).searchParams;
 				const inputParams = {};
 				for (const [key, value] of urlParams) {
@@ -302,10 +522,7 @@ const server = http.createServer(async (request, response) => {
 				}
 				_putDataInDB(serverData)
 
-				response.setHeader('Access-Control-Allow-Origin', '*')
-				response.setHeader('Access-Control-Allow-Methods', '*')
-				response.setHeader('Access-Control-Allow-Headers', '*')
-				response.setHeader('Access-Control-Max-Age', 2592000)
+				_setHeaders(response)
 				response.end()
 				return
 			})
